2d58126ae0c96d72714367c989a5e7127eccf288
Byronodhiambo <byronjonnes25@gmail.com>
2022-02-26 18:23:20 +0300

    updates

--- BTL/settings.py
+++ BTL/settings.py
@@ -126,9 +126,15 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'smartpatrol/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'BTL/media')
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
