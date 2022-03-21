7f21a01073ac70f7f8362f21a408997adb355628
Byronodhiambo <byronjonnes25@gmail.com>
2022-03-22 00:02:09 +0300

    form

--- be_the_leader/urls.py
+++ be_the_leader/urls.py
@@ -1,22 +1,19 @@
 from posixpath import basename
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
+
 
 
 app_name = 'be_the_leader'
 
 urlpatterns = [
-    # path('viewset/', include(router.urls)),
-    # path('viewset/<int:pk>', include(router.urls)),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('what_we_do/', views.what_we_do, name='what_we_do'),
     path('gallery/', views.gallery, name='gallery'),
     path('contact/', views.contact, name='contact'),
-    path('feedback/', views.feedback, name='feedback'),
-    
+    path('feedback/', views.feedback, name='feedback'),   
 
 
 
